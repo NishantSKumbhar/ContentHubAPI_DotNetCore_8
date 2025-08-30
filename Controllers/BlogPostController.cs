26726c94b1efb95f41369f8d90cda1f3c578cecf
NishantSKumbhar <nishantsk210@gmail.com>
2025-08-30 17:02:58 +0530

    Add project files.

---
+++ Controllers/BlogPostController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ContentHubAPI.Controllers
+{
+    //https://localhost:7157/api/BlogPost
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BlogPostController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetAllBlogPosts()
+        {
+            var posts = new string[] { "Blog1", "Blog2", "Blog3" };
+            return Ok(posts);
+        }
+    }
+}
