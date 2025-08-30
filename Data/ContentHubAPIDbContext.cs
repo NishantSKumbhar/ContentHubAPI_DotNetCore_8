a36f9ab7d275d4ed071ab1db1c3a2037205f2ddf
NishantSKumbhar <nishantsk210@gmail.com>
2025-08-30 18:14:01 +0530

    Added EF core, SQL serer and Tool Nuget packages and creatd DbContext class

---
+++ Data/ContentHubAPIDbContext.cs
@@ -0,0 +1,18 @@
+﻿using ContentHubAPI.Models.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace ContentHubAPI.Data
+{
+    public class ContentHubAPIDbContext: DbContext
+    {
+        public ContentHubAPIDbContext(DbContextOptions dbContextOptions):base(dbContextOptions)
+        {
+            
+        }
+
+        //when we run entity framework commands, it will create tables for these models
+        public DbSet<CategoryDomainModel> Categories { get; set; }
+        public DbSet<UserDomainModel> Users { get; set; }
+        public DbSet<PostDomainModel> Posts { get; set; }
+    }
+}
